fc81a42a5b8098c8c534a1fbda390cc732d892d7
143yashchouhan <143yashchouhan@gmail.com>
2022-12-16 15:16:17 +0530

    date program

---
+++ date.java
@@ -0,0 +1,17 @@
+package JavaPrograms;
+
+import java.util.*;
+
+public class date {
+    public static void main(String[] args) {
+        int month, days, input;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter No. of Days : ");
+        input = sc.nextInt();
+        month = input / 30;
+        System.out.println("Months : " + month);
+        days = input - (month * 30);
+        System.out.println("Days : " + days);
+
+    }
+}
