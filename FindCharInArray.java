1cc44900c0b83681f4bccd57f1413f302ce6a178
143yashchouhan <143yashchouhan@gmail.com>
2022-12-29 19:30:02 +0530

    Char in Array

---
+++ FindCharInArray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class FindCharInArray {
+    public static void main(String[] args) {
+        String sent = "java is programming language and I love java";
+        char find = 'o';
+        int i, times = 0;
+        for(i = 0; i < sent.length(); i++ ){
+            if(sent.charAt(i) == find){
+                times++;
+            }
+        }
+        System.out.println("O Printed : " + times);
+    }
+}
